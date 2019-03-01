cfa9f9f1c238c968fb83093dac01794047f0f9e3
Shatki <Shatki@mail.ru>
2019-03-01 09:16:14 +0300

    Разработка галлереи gallery_list

---
+++ Tatyana/constants.py
@@ -0,0 +1,16 @@
+# Корневое меню
+ABOUT = 'about'
+GROUP = 'group'
+DOCS = 'docs'
+NEWS = 'news'
+CONTACTS = 'contacts'
+
+MENU_DEFAULT = ABOUT
+
+MENU_CHOICES = (
+    (ABOUT, 'обо мне'),
+    (GROUP, 'наша группа'),
+    (DOCS, 'документы'),
+    (NEWS, 'новости'),
+    (CONTACTS, 'контакты'),
+)
